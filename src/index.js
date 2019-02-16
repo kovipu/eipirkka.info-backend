83a0a130f373a7abaf33f6bc941c4f272d457405
Konsta Purtsi <kovipu@utu.fi>
2019-02-16 02:51:57 +0200

    Send image to AutoML. Still throws 'Requested entity was not found.'

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 const express = require('express');
-const VisionService = require('./services/VisionService');
 const multer = require('multer');
+
+const VisionService = require('./services/VisionService');
 
 const app = express();
 const visionService = VisionService();
-const upload = multer({dest: 'tmp/'});
+const upload = multer({ dest: 'tmp/' });
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-// 
-app.post('/image', upload.single('image'), (req, res) => {
-  console.log(req.body);
+app.post('/predict', upload.single('image'), (req, res) => {
   const base64data = req.file;
-  const response = visionService.analyzeImage(base64data);
-  res.json({
-    analyzedAt: new Date().toDateString(),
-    response
-  })
-})
+  visionService.analyzeImage(base64data)
+  res.send('OK')
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
--- src/services/VisionService.js
+++ src/services/VisionService.js
@@ -1,11 +1,31 @@
 const automl = require('@google-cloud/automl');
+const fs = require('fs');
+
+const { PROJECT_ID, REGION_NAME, MODEL_ID } = process.env;
 
 function VisionService() {
-  const autoMLClient = new automl.AutoMlClient();
+  const predictionClient = new automl.PredictionServiceClient();
+  const name = predictionClient.modelPath(PROJECT_ID, REGION_NAME, MODEL_ID);
+
+  console.log('name:', name)
 
   function analyzeImage(data) {
-    console.log('Data in request is', data);
-    return true;
+    if (!data.path) {
+      console.log('No file received');
+      return { success: false };
+    }
+
+    const file = fs.readFileSync(data.path);
+
+    const payload = {
+      image: {
+        imageBytes: file.toString('base64')
+      }
+    };
+
+    predictionClient.predict({ name, payload })
+      .then(res => console.log('Response: ', res))
+      .catch(err => console.log('Error: ', err));
   }
   return {
     analyzeImage
